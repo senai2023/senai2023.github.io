80be2dd34ce02af26d08c7016d6fd73c3f37548f
senai2023 <129984592+senai2023@users.noreply.github.com>
2023-04-13 16:53:05 -0300

    Update a2.js

--- a2.js
+++ a2.js
@@ -1,58 +1,10 @@
-const display = document.querySelector('.display');
-const numberBtns = document.querySelectorAll('.number');
-const operatorBtns = document.querySelectorAll('.operator');
-const clearBtn = document.querySelector('#clear');
-const equalBtn = document.querySelector('#equal');
+const loginButton = document.getElementById("login-button");
 
-let firstNumber = null;
-let operator = null;
+loginButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  loginButton.style.transform = "scale(0.9)";
+  setTimeout(function() {
+    loginButton.style.transform = "scale(1)";
+  }, 200);
+});
 
-function clearDisplay() {
-  display.value = '';
-}
-
-function calculate() {
-  const secondNumber = parseFloat(display.value);
-  let result = null;
-  switch (operator) {
-    case '+':
-      result = firstNumber + secondNumber;
-      break;
-    case '-':
-      result = firstNumber - secondNumber;
-      break;
-    case '*':
-      result = firstNumber * secondNumber;
-      break;
-    case '/':
-      result = firstNumber / secondNumber;
-      break;
-    default:
-      result = secondNumber;
-  }
-  display.value = result;
-  firstNumber = result;
-  operator = null;
-}
-
-function handleNumberClick(event) {
-  const digit = event.target.value;
-  display.value += digit;
-}
-
-function handleOperatorClick(event) {
-  const op = event.target.value;
-  if (firstNumber === null) {
-    firstNumber = parseFloat(display.value);
-    operator = op;
-    clearDisplay();
-  } else {
-    calculate();
-    operator = op;
-  }
-}
-
-numberBtns.forEach(btn => btn.addEventListener('click', handleNumberClick));
-operatorBtns.forEach(btn => btn.addEventListener('click', handleOperatorClick));
-clearBtn.addEventListener('click', clearDisplay);
-equalBtn.addEventListener('click', calculate);
